b04c0972b708c32ed27e73e587b156ce43e976b1
Marcos-Carros-Amorim <71189647+Marcos-Carros-Amorim@users.noreply.github.com>
2023-10-16 21:56:53 -0300

    Update EChardcode16-10.asm

--- EChardcode16-10.asm
+++ EChardcode16-10.asm
@@ -1,33 +1,33 @@
 section .data
- msg1 db 'a soma é:'
- len equ $ -msg1
- 
- section .bss
-  res resb 1
-  
-  global _start
-_start
-mov eax, 3
-mov ebx, 4
+    msg1 db 'A soma é: ';ele pega uma variavel
+    len equ $ - msg1
 
-add eax, ebx
+section .bss
+    res resb 1
 
-add eax, '0'
+section .text
+global _start
 
-mov [res], eax
+_start:
+    mov eax, 3
+    add eax, 4
 
-mov eax, 4
-mov ebx, 1
-mov ecx, msg1
-mov edx, ven
-int 0x80
+    add eax, '0'
 
-mov eax, 4
-mov ebx, 1
-mov ecx, res
-mov edx, 2
+    mov [res], eax
+; Escreva a mensagem "a soma é: "
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg1
+    mov edx, len
+    int 0x80
+; Escreva o resultado
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, res
+    mov edx, 2
 
-exit:
-mov eax, 1
-xor ebx, ebx
-int 0x80
+exit: ; Saia do programa
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
