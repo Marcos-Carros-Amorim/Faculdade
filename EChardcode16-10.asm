a5941b908bbfce0403b8017d2209129b1bf5b596
Marcos-Carros-Amorim <71189647+Marcos-Carros-Amorim@users.noreply.github.com>
2023-10-16 21:36:49 -0300

    Update EChardcode16-10.asm

--- EChardcode16-10.asm
+++ EChardcode16-10.asm
@@ -1,1 +1,33 @@
+section .data
+ msg1 db 'a soma é:'
+ len equ $ -msg1
+ 
+ section .bss
+  res resb 1
+  
+  global _start
+_start
+mov eax, 3
+mov ebx, 4
 
+add eax, ebx
+
+add eax, '0'
+
+mov [res], eax
+
+mov eax, 4
+mov ebx, 1
+mov ecx, msg1
+mov edx, ven
+int 0x80
+
+mov eax, 4
+mov ebx, 1
+mov ecx, res
+mov edx, 2
+
+exit:
+mov eax, 1
+xor ebx, ebx
+int 0x80
